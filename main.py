80e6857b7e3c6de1c2ed05f2f7166e3447e76206
Yue Yang <yueyang2333@gmail.com>
2019-09-03 19:53:54 -0400

    Sync

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from FlagCapture import FlagCaptureGraph
 V = [(0, 0), (0, 1), (0, 2), (0, 3), (1, 0), (1, 1), (1, 2), (1, 3), (2, 0), (2, 1), (2, 2), (2, 3), (3, 0), (3, 1), (3, 2), (3, 3)]
 E = [((0, 0), (0, 1)), ((0, 0), (1, 0)), ((0, 1), (0, 2)), ((0, 1), (1, 1)), ((0, 1), (0, 0)), ((0, 2), (0, 3)), ((0, 2), (1, 2)), ((0, 2), (0, 1)), ((0, 3), (1, 3)), ((0, 3), (0, 2)), ((1, 0), (0, 0)), ((1, 0), (1, 1)), ((1, 0), (2, 0)), ((1, 1), (0, 1)), ((1, 1), (1, 2)), ((1, 1), (2, 1)), ((1, 1), (1, 0)), ((1, 2), (0, 2)), ((1, 2), (1, 3)), ((1, 2), (2, 2)), ((1, 2), (1, 1)), ((1, 3), (0, 3)), ((1, 3), (2, 3)), ((1, 3), (1, 2)), ((2, 0), (1, 0)), ((2, 0), (2, 1)), ((2, 0), (3, 0)), ((2, 1), (1, 1)), ((2, 1), (2, 2)), ((2, 1), (3, 1)), ((2, 1), (2, 0)), ((2, 2), (1, 2)), ((2, 2), (2, 3)), ((2, 2), (3, 2)), ((2, 2), (2, 1)), ((2, 3), (1, 3)), ((2, 3), (3, 3)), ((2, 3), (2, 2)), ((3, 0), (2, 0)), ((3, 0), (3, 1)), ((3, 1), (2, 1)), ((3, 1), (3, 2)), ((3, 1), (3, 0)), ((3, 2), (2, 2)), ((3, 2), (3, 3)), ((3, 2), (3, 1)), ((3, 3), (2, 3)), ((3, 3), (3, 2))]
-state = [['D2_1', 'x', 'x', 'x'], ['D2_2', 'x', 'x', 'x'], ['x', 'x', 'x', 'Q5_2'], ['x', 'x', 'x', 'Q5_1']]
+state = [['D2_1', 'x', 'x', 'x'], ['D2_2', 'x', 'x', 'x'], ['x', 'x', 'x', 'Q5_1'], ['x', 'x', 'x', 'Q5_2']]
 flag = {'flag_D2': (3, 2), 'flag_Q5': (0, 1)}
 graph = FlagCaptureGraph(V, E, state, flag)
 
@@ -15,41 +15,30 @@ while graph.game_over() != True:
 	print('Round: ' + str(Round))
 	print('D2 Turn')
 	D2_movement = graph.get_best_move(D2, limit)[0]
-	move_direction = graph.perform_move(graph.robot_pos['D2_1'], D2_movement['D2_1'])
-	record_game.append(('D2_1', move_direction))
+	move_robot = D2_movement[0]
+	print(move_robot)
+	move_direction = graph.perform_move(graph.robot_pos[move_robot], D2_movement[1])
+	record_game.append((move_robot, move_direction))
 	graph.printmap()
 	print('             ')
 	if graph.game_over():
 		print('D2 WIN')
 		break
-	move_direction = graph.perform_move(graph.robot_pos['D2_2'], D2_movement['D2_2'])
-	record_game.append(('D2_2', move_direction))
-	graph.printmap()
-	print('             ')
-	if graph.game_over():
-		print('D2 WIN')
-		break
+
 	print('Q5 Turn')
-	print(graph.get_best_move(False, limit))
 	Q5_movement = graph.get_best_move(False, limit)[0]
-	move_direction = graph.perform_move(graph.robot_pos['Q5_1'], Q5_movement['Q5_1'])
-	record_game.append(('Q5_1', move_direction))
-	graph.printmap()
-	print('             ')
-	if graph.game_over():
-		print('Q5 WIN')
-		break
-	move_direction = graph.perform_move(graph.robot_pos['Q5_2'], Q5_movement['Q5_2'])
-	record_game.append(('Q5_2', move_direction))
+	move_robot = Q5_movement[0]
+	move_direction = graph.perform_move(graph.robot_pos[move_robot], Q5_movement[1])
+	record_game.append((move_robot, move_direction))
 	graph.printmap()
 	print('             ')
 	if graph.game_over():
 		print('Q5 WIN')
 		break
 	Round += 1
-	if Round > 20:
+	if Round > 40:
 		break
-
+'''
 while graph.game_over() != True:
 	print('Round: ' + str(Round))
 	print('Q5 Turn')
@@ -97,6 +86,10 @@ time = 1.5
 action(record_game, Droids, speed, time)
 for key in Droids:
 	Droids[key].disconnect()
+<<<<<<< HEAD
+=======
+action(record_game, Droids, speed, time)'''
+>>>>>>> 8a70f858d323daf2ffe7f22b512e72d0c594ca06
 
 
 
