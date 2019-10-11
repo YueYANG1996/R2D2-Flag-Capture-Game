58c35fcce715b91bb23fe3b838088eb288780f96
Yue Yang <yueyang2333@gmail.com>
2019-10-11 18:27:46 -0400

    improve algorithm

--- FlagCapture.py
+++ FlagCapture.py
@@ -5,25 +5,19 @@ from collections import defaultdict
 inf = float('inf')
 
 class FlagCaptureGraph:
-    def __init__(self, V, E, initial_state, flag):
+    def __init__(self, V, E, robots_pos, flags_pos):
         self.vertics = V
         self.edges = E
-        self.map = initial_state
-        self.state = {}
-        self.robot_pos = {}
-        for vertic in self.vertics:
-            if initial_state[vertic[0]][vertic[1]] != 'x':
-                self.robot_pos[initial_state[vertic[0]][vertic[1]]] = vertic
-            self.state[vertic] = initial_state[vertic[0]][vertic[1]]
-        self.flag = flag
-
-
+        self.flags_pos = flags_pos
+        self.robots_pos = robots_pos
+        self.direct2vec = {'south': (1, 0), 'north': (-1, 0), 'east': (0, 1), 'west': (0, -1), 'stay': (0, 0)}
+        
     def neighbors(self, u):
         neighbors_lst = []
         for x, y in [(-1, 0), (0, 1), (1, 0), (0, -1)]:
             next_state = (u[0] + x, u[1] + y)
             if (u, next_state) in self.edges:
-                if self.state[next_state] == 'x':
+                if next_state not in self.robots_pos.values():
                     neighbors_lst.append(next_state)
         return neighbors_lst
 
@@ -45,7 +39,7 @@ class FlagCaptureGraph:
             current_node = q.get()
             astar_cost, current_cost, path, current_pos = current_node[0], current_node[1], current_node[2], current_node[3]
             
-            if self.state[goal] != 'x':
+            if goal in self.robots_pos.values():
                 if self.dist_between(current_pos, goal) == 1:
                     return (path, len(path) + 1)
 
@@ -63,127 +57,76 @@ class FlagCaptureGraph:
         return (path, len(path))
 
     def copy(self):
-        new_map = copy.deepcopy(self.map)
-        return FlagCaptureGraph(self.vertics, self.edges, new_map, self.flag)
-
+        new_robots_pos = copy.deepcopy(self.robots_pos)
+        new_flag_pos = copy.deepcopy(self.flags_pos)
+        return FlagCaptureGraph(self.vertics, self.edges, new_robots_pos, new_flag_pos)
 
     def game_over(self):
-        if self.robot_pos['D2_1'] == self.flag['flag_D2'] or self.robot_pos['D2_2'] == self.flag['flag_D2'] or self.robot_pos['Q5_1'] == self.flag['flag_Q5'] or self.robot_pos['Q5_2'] == self.flag['flag_Q5']:
+        if self.robots_pos['D2_1'] == self.flags_pos['flag_D2'] or self.robots_pos['D2_2'] == self.flags_pos['flag_D2'] or self.robots_pos['Q5_1'] == self.flags_pos['flag_Q5'] or self.robots_pos['Q5_2'] == self.flags_pos['flag_Q5']:
             return True
         else:
             return False
 
-    def printmap(self):
-        for i in range(len(self.map)):
-            row_str = ''
-            for j in range(len(self.map[0])):
-                grid = self.map[i][j]
-                if grid == 'D2_1' or grid == 'D2_2':
-                    add = '○ '
-                elif grid == 'Q5_1' or grid == 'Q5_2':
-                    add = '• '
-                elif self.flag['flag_D2'] == (i, j):
-                    add = '◘ '
-                elif self.flag['flag_Q5'] == (i, j):
-                    add = '◙ '
-                else:
-                    add = '☐ '
-                row_str += add
-            print(row_str)
+    def islegalmove(self, move_robot, move_direction):
+        current_pos = self.robots_pos[move_robot]
+        movement = self.direct2vec[move_direction]
+        next_pos = (current_pos[0] + movement[0], current_pos[1] + movement[1])
+        if next_pos in self.robots_pos.values():
+            return False
+        else:
+            if (current_pos, next_pos) in self.edges:
+                return True
+            else:
+                return False
 
-    '''Rule 1: Move 2 robots each turn'''
+    def legalmoves(self, move_robot):
+        result = []
+        for move_direction in ['south', 'north', 'east', 'west']:
+            if self.islegalmove(move_robot, move_direction):
+                result.append(move_direction)
+        if len(move_direction) == 0:
+            result.append('stay')
+        return result
+
+    def perform_move(self, move_robot, move_direction):
+        if self.islegalmove(move_robot, move_direction) == False:
+            return None
+        movement = self.direct2vec[move_direction]
+        current_pos = self.robots_pos[move_robot]
+        self.robots_pos[move_robot] = (current_pos[0] + movement[0], current_pos[1] + movement[1])
+
     def successors(self, D2):
         if D2 == True:
-            D2_1_current_state = self.robot_pos['D2_1']
-            for D2_1_next_state in self.neighbors(D2_1_current_state):
-                move = {}
-                move['D2_1'] = D2_1_next_state
+            for move_direction_1 in self.legalmoves('D2_1'):
+                movement = {}
+                movement['D2_1'] = move_direction_1
                 new_game_1 = self.copy()
-                new_game_1.perform_move(D2_1_current_state, D2_1_next_state)
-                D2_2_current_state = new_game_1.robot_pos['D2_2']
-
-                for D2_2_next_state in new_game_1.neighbors(D2_2_current_state):
-                    move['D2_2'] = D2_2_next_state
+                new_game_1.perform_move('D2_1', move_direction_1)
+                for move_direction_2 in new_game_1.legalmoves('D2_2'):
+                    movement['D2_2'] = move_direction_2
                     new_game_2 = new_game_1.copy()
-                    new_game_2.perform_move(D2_2_current_state, D2_2_next_state)
-                    yield move, new_game_2
+                    new_game_2.perform_move('D2_2', move_direction_2)
+                    yield movement, new_game_2
         else:
-            Q5_1_current_state = self.robot_pos['Q5_1']
-            for Q5_1_next_state in self.neighbors(Q5_1_current_state):
-                move = {}
-                move['Q5_1'] = Q5_1_next_state
+            for move_direction_1 in self.legalmoves('Q5_1'):
+                movement = {}
+                movement['Q5_1'] = move_direction_1
                 new_game_1 = self.copy()
-                new_game_1.perform_move(Q5_1_current_state, Q5_1_next_state)
-                Q5_2_current_state = new_game_1.robot_pos['Q5_2']
-
-                for Q5_2_next_state in new_game_1.neighbors(Q5_2_current_state):
-                    move['Q5_2'] = Q5_2_next_state
+                new_game_1.perform_move('Q5_1', move_direction_1)
+                for move_direction_2 in new_game_1.legalmoves('Q5_2'):
+                    movement['Q5_2'] = move_direction_2
                     new_game_2 = new_game_1.copy()
-                    new_game_2.perform_move(Q5_2_current_state, Q5_2_next_state)
-                    yield move, new_game_2
-
-    '''Rule 2: Choose one robot to move each turn'''
-    '''def successors(self, D2):
-        if D2 == True:
-            D2_1_current_state = self.robot_pos['D2_1']
-            D2_2_current_state = self.robot_pos['D2_2']
-
-            for D2_1_next_state in self.neighbors(D2_1_current_state):
-                move = ['D2_1', D2_1_next_state]
-                new_game_1 = self.copy()
-                new_game_1.perform_move(D2_1_current_state, D2_1_next_state)
-                yield move, new_game_1
-
-            for D2_2_next_state in self.neighbors(D2_2_current_state):
-                move = ['D2_2', D2_2_next_state]
-                new_game_2 = self.copy()
-                new_game_2.perform_move(D2_2_current_state, D2_2_next_state)
-                yield move, new_game_2
-        else:
-            Q5_1_current_state = self.robot_pos['Q5_1']
-            Q5_2_current_state = self.robot_pos['Q5_2']
-            for Q5_1_next_state in self.neighbors(Q5_1_current_state):
-                move = ['Q5_1', Q5_1_next_state]
-                new_game_1 = self.copy()
-                new_game_1.perform_move(Q5_1_current_state, Q5_1_next_state)
-                yield move, new_game_1
-
-            for Q5_2_next_state in self.neighbors(Q5_2_current_state):
-                move = ['Q5_2', Q5_2_next_state]
-                new_game_2 = self.copy()
-                new_game_2.perform_move(Q5_2_current_state, Q5_2_next_state)
-                yield move, new_game_2'''
-
-    def perform_move(self, current_state, move_state):
-        vector = (move_state[0] - current_state[0], move_state[1] - current_state[1])
-        if vector == (1, 0):
-            movement = 'south'
-        elif vector == (0, 1):
-            movement = 'east'
-        elif vector == (0, -1):
-            movement = 'west'
-        elif vector == (-1, 0):
-            movement = 'north'
-        else:
-            movement = 'stay'
-        move_robot = self.state[current_state]
-        self.state[move_state] = move_robot
-        self.state[current_state] = 'x'
-        self.robot_pos[move_robot] = move_state
-        self.map[current_state[0]][current_state[1]] = 'x'
-        self.map[move_state[0]][move_state[1]] = move_robot
-        return movement
+                    new_game_2.perform_move('Q5_2', move_direction_2)
+                    yield movement, new_game_2
 
     def evaluate(self, D2):
-        cost_D2_1 = self.Astar(self.robot_pos['D2_1'], self.flag['flag_D2'])[1]
-        cost_D2_2 = self.Astar(self.robot_pos['D2_2'], self.flag['flag_D2'])[1]
-        cost_Q5_1 = self.Astar(self.robot_pos['Q5_1'], self.flag['flag_Q5'])[1]
-        cost_Q5_2 = self.Astar(self.robot_pos['Q5_2'], self.flag['flag_Q5'])[1]
+        cost_D2_1 = self.Astar(self.robots_pos['D2_1'], self.flags_pos['flag_D2'])[1]
+        cost_D2_2 = self.Astar(self.robots_pos['D2_2'], self.flags_pos['flag_D2'])[1]
+        cost_Q5_1 = self.Astar(self.robots_pos['Q5_1'], self.flags_pos['flag_Q5'])[1]
+        cost_Q5_2 = self.Astar(self.robots_pos['Q5_2'], self.flags_pos['flag_Q5'])[1]
         if D2 == True:
-            '''return (cost_Q5_1 + cost_Q5_2) - (cost_D2_1 + cost_D2_2)'''
             return min(cost_Q5_1, cost_Q5_2) - min(cost_D2_1, cost_D2_2)
         else:
-            '''return -(cost_Q5_1 + cost_Q5_2) + (cost_D2_1 + cost_D2_2)'''
             return - min(cost_Q5_1, cost_Q5_2) + min(cost_D2_1, cost_D2_2)
 
     def alpha_beta_max(self, D2, original_D2, limit, alpha, beta):
@@ -219,3 +162,4 @@ class FlagCaptureGraph:
     def get_best_move(self, D2, limit):
         return self.alpha_beta_max(D2, D2, limit, float("-inf"),
             float("inf"))
+        
